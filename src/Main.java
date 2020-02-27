e1e051fe9c6896d2c3745721341a9768ebcf8305
rocket7642 <49769828+rocket7642@users.noreply.github.com>
2020-02-26 18:10:24 -0800

    Update Main.java

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,13 @@
+package src;
+
 import org.junit.Test;
 import java.util.Arrays;
 import static org.junit.Assert.*;
 
 public class Main {
+    
+    //well for what ever reason i am having a brain fart and cannot figure it out so i will submit
+    //what has been done
 
     public static void main(String[] args) {
         // left empty on purpose
@@ -19,18 +24,20 @@ public class Main {
         assertTrue(Arrays.equals(inputs, expects));
     }
 
-	// to sort in ascending order
+    // to sort in ascending order
     public static void bubbleSort(int[] data) {
-		// check data size
-		if (data.length>0) {
-			return;
-		}
+        // check data size
+        if (data.length>0) {
+            return;
+        }
 
-		// sort
+        // sort
         for (int i=0; i<data.length; i++) {
-            for (int j=0; j<data.length-i-1; j++) {
-                if (data[j]>data[j+1]) {
-                    swap(data, j);
+            for (int j=0; j<data.length; j++) {
+                if (data[j]>data[j+1] && (j+1) != data.length) {
+
+                    swap(data, j); 
+
                 }
             }
         }
@@ -38,7 +45,8 @@ public class Main {
 
     private static void swap(int[] data, int j) {
         int k = data[j];
-        data[j] = data[j+1];
+        int f = data[j+1];
+        data[j] = f;
         data[j+1] = k;
     }
 }
